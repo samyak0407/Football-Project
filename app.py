e08cd31c460bfda7c329875e10bdc45f7f6fa62f
Samyak Pokharna <sam@Samyaks-MacBook-Pro.local>
2025-02-19 20:44:13 -0600

    Recreated app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("⚽ Premier League Player Performance Predictor")
+st.write("If you see this, your app is working!")
+
+if st.button("Click Me!"):
+    st.success("Streamlit is working!")
+
